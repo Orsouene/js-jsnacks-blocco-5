46a181f5db650d768e562b09e2c8499012a0818c
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-15 17:39:33 +0100

    snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,13 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const myString = people.map((element) => {
+  return "'" + element.name + "'";
+});
+console.log(" Risultato: " + myString.join(","));
